230257fe638ea1d7fb3bcfa075ce26fa6bdee11b
rochafialvin <rochafi.dev@gmail.com>
2020-05-04 11:21:21 +0700

    exercise register

--- src/theory/basic-nodemailer.js
+++ src/theory/basic-nodemailer.js
@@ -2,30 +2,33 @@ const nodemailer = require('nodemailer')
 const dotenv = require('dotenv')
 dotenv.config()
 
-// Config
-const transporter = nodemailer.createTransport({
-   service: 'gmail',
-   auth : {
-      type: 'OAuth2',
-      user: 'rochafi.teach@gmail.com',
-      clientId : process.env.CLIENT_ID,
-      clientSecret : process.env.CLIENT_SECRET,
-      refreshToken : process.env.REFRESH_TOKEN
-   }
-})
+const verifSendEmail = () => {
+   // Config
+   const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth : {
+         type: 'OAuth2',
+         user: 'rochafi.teach@gmail.com',
+         clientId : process.env.CLIENT_ID,
+         clientSecret : process.env.CLIENT_SECRET,
+         refreshToken : process.env.REFRESH_TOKEN
+      }
+   })
 
-// Mail
-const mail = {
-   from : 'Rochafi Teach <rochafi.teach@gmail.com',
-   to: 'rochafi.dev@gmail.com',
-   subject: 'Testing Nodemailer',
-   html: '<h1>Alhamdulillah berhasil</h1>'
+   // Mail
+   const mail = {
+      from : 'Rochafi Teach <rochafi.teach@gmail.com',
+      to: 'rochafi.dev@gmail.com',
+      subject: 'Testing Nodemailer',
+      html: '<h1>Alhamdulillah berhasil</h1>'
+   }
+
+   // Send Email
+   transporter.sendMail(mail, (err, result) => {
+      if(err) return console.log({errornich: err.message})
+
+      console.log('Email terkirim')
+   })
 }
 
-// Send Email
-
-transporter.sendMail(mail, (err, result) => {
-   if(err) return console.log({errornich: err.message})
-
-   console.log('Email terkirim')
-})
+module.exports = verifSendEmail
